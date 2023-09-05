33868c41df1b7eeb38a61f56ee78937b13ebe764
yali <yusufali0147@hotmail.com>
2023-09-05 10:11:34 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Amstrong Sayı bulma");
+         Scanner imp= new Scanner(System.in);
+        System.out.print("Sayı giriniz : ");
+        int number = imp.nextInt();
+
+        int result=0,pow,temp=number,basvalue,basnumber=0;
+        while (temp!=0) {
+            temp/=10;
+            basnumber++;
+        }temp=number;
+        while (temp!=0){
+            basvalue = temp % 10;
+            pow=1;
+            for (int i=1;i<=basnumber;i++){
+                pow*=basvalue;
+            }
+            result+=pow;
+            temp/=10;
+        }
+        if (result == number) {
+            System.out.println(number + " sayısı bir Armstrong sayıdır.");
+        } else {
+            System.out.println(number + " sayısı bir Armstrong sayısı değildir.");
+        }
+
     }
 }
